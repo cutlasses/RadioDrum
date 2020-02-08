9e3a64276759afe237b1d6affe56a8c26ef744ad
Cutlasses <spitkethly@gmail.com>
2020-02-08 15:08:57 +0000

    Adding drum samples

---
+++ AudioSampleAddhit1.h
@@ -0,0 +1,3 @@
+// Audio data converted from WAV file by wav2sketch
+
+extern const unsigned int AudioSampleAddhit1[1825];
--- AudioSamplePiano_c3_44k.h
+++
@@ -1,3 +0,0 @@
-// Audio data converted from WAV file by wav2sketch
-
-extern const unsigned int AudioSamplePiano_c3_44k[54465];
